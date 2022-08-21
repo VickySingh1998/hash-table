ac4b90841e9c950a88276a85e2e46179bba5dbc8
vickysingh1998 <vickysingh8697@gmail.com>
2022-08-21 19:43:07 +0530

    added: function to find frequency of words

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -5,7 +5,20 @@ import java.util.Hashtable;
 public class HashTable {
     public static void main(String[] args) {
 
-        System.out.println("Welcome To Hash Table Program");
+        Hashtable<String, Integer> hashTable = new Hashtable<>();
+        String message = "To be or not to be";
 
+        String[] messageArray = message.toLowerCase().split(" ");
+
+        for (String word : messageArray){
+            Integer value = hashTable.get(word);
+
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashTable.put(word, value);
+        }
+        System.out.println(hashTable);
     }
 }
